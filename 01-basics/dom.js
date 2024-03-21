28524bf7731074490270d182a2f3c26c2dd94e53
hassanahmed787 <164336027+hassanahmed787@users.noreply.github.com>
2024-03-21 11:11:05 +0000

    updated

---
+++ 01-basics/dom.js
@@ -0,0 +1,5 @@
+console.log('ahmed hey')
+document.body.getElementsByTagName('p')[0].style.backgroundColor='green'
+document.body.getElementsByTagName('p')[0].style.color='white'
+document.body.getElementsByTagName('p')[0].style.fontSize='50px'
+document.body.getElementsByTagName('p')[0].style.textAlign='center'
